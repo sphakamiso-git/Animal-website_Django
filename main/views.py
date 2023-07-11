2c60ba3fe79df747f31cfd552c5864065f2cc3b9
sphakamiso <sparngobese@gmail.com>
2023-07-11 06:31:59 +0200

    added navigation links in the navigation bar and functionality to delete

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Article, ArticleSeries
 
+from users.decorators import user_is_superuser
+from .forms import SeriesCreateForm, ArticleCreateForm, SeriesUpdateForm, ArticleUpdateForm
+
 
 def homepage(request):
     matching_series = ArticleSeries.objects.all()
@@ -19,8 +22,16 @@ def article(request, series: str, article:str):
 
     return render(request=request, template_name='main/article.html', context={'object': matching_article})
 
+@user_is_superuser
 def new_series(request):
-    return redirect('/')
+    if request.method == "POST":
+        form = SeriesCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+        else:
+            form = SeriesCreateForm()
+    return render(request=request, template_name='main/new_record.html', context={'object': form})
 
 def new_post(request):
     return redirect('/')
